aa67948a9646614ac81cd1479cd45986c413cf63
karansaini95 <129549612+karansaini95@users.noreply.github.com>
2023-06-06 19:27:51 -0700

    sync

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
+      @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = current_user["id"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
-  end
+    end
 
 end
